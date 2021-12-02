1f122dfdb9df3ea232897178385d443ad6fc11b6
Toms Druvaskalns <tomsdruvas@gmail.com>
2021-12-02 09:27:17 +0000

    last

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,9 +2,11 @@ from app import app
 from flask import render_template
 from models.orders import list_of_orders
 
-
-
 @app.route('/')
+def index_one():
+    return render_template('index.html', title = "Orders", list_of_orders = list_of_orders)
+
+@app.route('/order/')
 def index():
     return render_template('index.html', title = "Orders", list_of_orders = list_of_orders)
 
@@ -13,4 +15,15 @@ def index():
 def order(order_id):
     order_id -= 1
     return render_template('order.html', order_num = order_id +1, order = list_of_orders[order_id])
+
+
+
+@app.route('/samepage/')
+def samepage():
+    return render_template('samepage.html', title = "Orders", list_of_orders = list_of_orders)
+
+@app.route('/samepage/<int:order_id>')
+def order1(order_id):
+    order_id -= 1
+    return render_template('samepage2.html', order_num = order_id +1, list_of_orders = list_of_orders, order = list_of_orders[order_id])
 
